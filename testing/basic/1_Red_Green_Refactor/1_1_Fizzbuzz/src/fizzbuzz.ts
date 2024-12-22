e1d9598a94ebcf992c473bdf8c37cf145d598f00
nischal puri <mrnischalpuri@gmail.com>
2024-12-22 09:15:27 +0545

    [red]: return 'Fizz for 5'

--- testing/basic/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ testing/basic/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,3 +1,18 @@
+// 3 returns "Fizz"
+// 5 returns "Buzz
+// 15 returns "FizzBuzz"
+// 9 returns "Fizz"
+// 43 returns "43"
+// 42 returns "Fizz"
+// 45 returns "FizzBuzz"
 export default function fizzbuzz(num: number) {
-  return "string"
+  return num === 3 || num == 9 || num == 42
+    ? "Fizz"
+    : num === 5
+    ? "Buzz"
+    : num === 15 || num === 45
+    ? "Fizzbuzz"
+    : num === 43
+    ? "43"
+    : ""
 }
--- testing/basic/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ testing/basic/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,6 +1,21 @@
 import fizzbuzz from "./fizzbuzz"
+// each return value is a string
+// 3 returns "Fizz"
+// 5 returns "Buzz
+// 15 returns "FizzBuzz"
+// 9 returns "Fizz"
+// 43 returns "43"
+// 42 returns "Fizz"
+// 45 returns "FizzBuzz"
+// 102 (you decide, throw an Error or handle some other way)
+// -12 (you decide, throw an Error or handle some other way)
+// any non-number (you decide, throw an Error or handle some other way)
+
 describe("fizzbuzz", () => {
   it("return a string", () => {
-    expect(typeof fizzbuzz(5)).toBe("string")
+    expect(fizzbuzz(3)).toBe("Fizz")
+  })
+  it("return a string", () => {
+    expect(fizzbuzz(5)).toBe("Fizz")
   })
 })
